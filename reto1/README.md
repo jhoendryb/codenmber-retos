7ea6abacfdbb9e456e68fbe774c3bcc55814e2a1
JhoendryB <jhoendryblanco@hotmail.com>
2023-11-04 04:03:45 -0400

    Reto 1

---
+++ reto1/README.md
@@ -0,0 +1,57 @@
+# Reto #01
+
+Un espía está enviando mensajes encriptados.
+
+Tu misión es crear un programa que nos ayude a buscar patrones...
+
+Los mensajes son palabras separadas por espacios como este:
+gato perro perro coche Gato peRRo sol
+
+Necesitamos que el programa nos devuelva el número de veces que aparece cada palabra en el mensaje, independientemente de si está en mayúsculas o minúsculas.
+
+El resultado será una cadena de texto con la palabra y el número de veces que aparece en el mensaje, con este formato:
+`gato2perro3coche1sol1`
+
+¡Las palabras son ordenadas por su primera aparición en el mensaje!
+
+** Más ejemplos: **
+
+```bash
+llaveS casa CASA casa llaves -> llaves2casa3
+taza ta za taza -> taza2ta1za1
+casas casa casasas -> casas1casa1casas1
+```
+
+## Solución
+
+```js
+function cuentaPalabras(texto) {
+    const palabras = texto.split(" ")
+        // PARSEAMOS EL TEXTO A MINUSCULAS
+        .map(plabra => plabra.toLowerCase());
+
+    let countPalabra = [ /* palabra: count */];
+
+    palabras.forEach(palabra => {
+        if (Object.keys(countPalabra).indexOf(palabra) === -1) {
+            // VALIDAMOS SI LA PALABRA NO HA SIDO AGREGADA AL CONTADOR
+            countPalabra[palabra] = 1;
+        } else if (Object.keys(countPalabra).indexOf(palabra) > -1) {
+            // EN CASO DE EXISTIR AUMENTAMOS SU CONTADOR
+            countPalabra[palabra]++;
+        }
+    })
+
+    // MAPEAMOS EL OBJETO DE LOS CONTADORES POR PALABRA
+    // PARA ORGANIZARLO EN UN ARRAY Y UNIRLOS EN UN SOLO STRING
+    return Object.entries(countPalabra)
+        .map(([clave, value]) => `${clave}${value}`)
+        .join('');
+}
+```
+
+## Respuesta
+
+```bash
+submit murcielago15leon15jirafa15cebra6elefante15rinoceronte15hipopotamo15ardilla15mapache15zorro15lobo15oso15puma2jaguar14tigre10leopardo10gato12perro12caballo14vaca14toro14cerdo14oveja14cabra14gallina10pato10ganso10pavo10paloma10halcon11aguila11buho11colibri9canario8loro8tucan8pinguino7flamenco7
+```
---
+++ reto1/index.js
@@ -0,0 +1,27 @@
+let texto = "murcielago leon jirafa cebra elefante rinoceronte hipopotamo ardilla mapache zorro lobo oso puma jaguar tigre leopardo gato perro caballo vaca toro cerdo oveja cabra gallina pato ganso pavo paloma halcon aguila buho colibri canario loro tucan pinguino flamenco tigre jaguar leopardo oso lobo zorro mapache ardilla elefante rinoceronte hipopotamo cebra jirafa leon murcielago cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago buho aguila halcon paloma pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago colibri buho aguila halcon paloma pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago tucan loro canario colibri buho aguila halcon paloma pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago flamenco pinguino tucan loro canario colibri buho aguila halcon paloma pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago jaguar oso lobo zorro mapache ardilla cebra elefante rinoceronte hipopotamo leon jirafa murcielago caballo vaca toro cerdo oveja cabra gallina pato ganso pavo paloma halcon aguila buho colibri canario loro tucan pinguino flamenco jaguar oso lobo zorro mapache ardilla cebra elefante rinoceronte hipopotamo leon jirafa murcielago caballo vaca toro cerdo oveja cabra gallina pato ganso pavo paloma halcon aguila buho colibri canario loro tucan pinguino flamenco murcielago leon jirafa cebra elefante rinoceronte hipopotamo ardilla mapache zorro lobo oso puma jaguar tigre leopardo gato perro caballo vaca toro cerdo oveja cabra gallina pato ganso pavo paloma halcon aguila buho colibri canario loro tucan pinguino flamenco oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla cebra elefante rinoceronte hipopotamo jirafa leon murcielago pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato buho aguila halcon paloma colibri canario loro tucan pinguino flamenco jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago cabra oveja cerdo toro vaca caballo perro gato buho aguila halcon paloma colibri canario loro tucan pinguino flamenco jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago cabra oveja cerdo toro vaca caballo perro gato buho aguila halcon";
+
+function cuentaPalabras(texto) {
+    const palabras = texto.split(" ")
+        // PARSEAMOS EL TEXTO A MINUSCULAS
+        .map(plabra => plabra.toLowerCase());
+
+    let countPalabra = [ /* palabra: count */];
+
+    palabras.forEach(palabra => {
+        if (Object.keys(countPalabra).indexOf(palabra) === -1) {
+            // VALIDAMOS SI LA PALABRA NO HA SIDO AGREGADA AL CONTADOR
+            countPalabra[palabra] = 1;
+        } else if (Object.keys(countPalabra).indexOf(palabra) > -1) {
+            // EN CASO DE EXISTIR AUMENTAMOS SU CONTADOR
+            countPalabra[palabra]++;
+        }
+    })
+
+    // MAPEAMOS EL OBJETO DE LOS CONTADORES POR PALABRA
+    // PARA ORGANIZARLO EN UN ARRAY Y UNIRLOS EN UN SOLO STRING
+    return Object.entries(countPalabra)
+        .map(([clave, value]) => `${clave}${value}`)
+        .join('');
+}
+
+console.log(cuentaPalabras(texto));
